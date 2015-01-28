b33e1d2dd77ec418d0cb4bfa774d36c5744fb1fa
Greg Babula <gbabula@gmail.com>
2015-01-28 15:04:43 -0500

    Additional test for new method in EventTower

--- test/g5-knockout-test-events-master.js
+++ test/g5-knockout-test-events-master.js
@@ -11,13 +11,14 @@
 var test          = require('tape');
 var masterEvents  = require('../src/scripts/events/master');
 
-test('g5-knockout test events', function(t) {
+test('g5-knockout events master test', function(t) {
 
-    t.plan(4);
+    t.plan(5);
 
     t.equal(typeof masterEvents.EventTower, 'function');
     t.equal(typeof masterEvents.EventTower().model, 'object');
     t.equal(typeof masterEvents.EventTower().viewModel, 'object');
     t.equal(typeof masterEvents.EventTower().attachEvents, 'function');
+    t.equal(typeof masterEvents.EventTower().detachEvents, 'function');
 
 });
