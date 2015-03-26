f0ee7e2e609da8ff47a163e558a4d5bce685eb23
Greg Babula <gbabula@gmail.com>
2015-03-25 23:14:41 -0400

    added demo component

---
+++ src/scripts/component/demo/demo-component.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @module DemoComponentViewModel
+ * @description simple baseline ViewModel
+ * @author Greg Babula
+ * 
+ */
+
+'use strict';
+
+/**
+ * 
+ * @constructor DemoComponentViewModel
+ * @param {Object} params
+ * 
+ */
+function DemoComponentViewModel(params) {
+
+    this.$data = params;
+
+}
+
+module.exports = DemoComponentViewModel;
